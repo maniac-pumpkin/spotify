5e7d31566a8303424b09a3978c69c01ebf29620d
Mohammad Mehdi Beheshti <mohammadmehdibeheshti@yahoo.com>
2023-12-17 18:29:35 +0330

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+// import { Route } from "wouter";
+
+export default function App() {
+	return (
+		<>
+			<h2 className="text-7xl">Nigger</h2>
+			<h2 className="text-6xl">Nigger</h2>
+			<h2 className="text-5xl">Nigger</h2>
+		</>
+	);
+}
