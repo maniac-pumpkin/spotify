8f84c20ce9f50dfdf36aca3d662e43518666209a
Mohammad Mehdi Beheshti <mohammadmehdibeheshti@yahoo.com>
2024-02-02 16:42:00 +0330

    No need for isMobile function

--- src/utils/isMobile.ts
+++
@@ -1,8 +0,0 @@
-const isMobile = () => {
-  const maxTouchPoints = navigator.maxTouchPoints > 0;
-  const userAgent = /Android|iPhone/i.test(navigator.userAgent);
-  const smallBreakPoint = innerWidth <= 576;
-  return maxTouchPoints || userAgent || smallBreakPoint;
-};
-
-export default isMobile;
