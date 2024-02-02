f53c3ac36c19776e4d9a5ea26aeb0dbf559e5bf9
Mohammad Mehdi Beheshti <mohammadmehdibeheshti@yahoo.com>
2024-02-02 16:46:27 +0330

    adding some features for playlist

--- src/services/apiPlaylist.ts
+++ src/services/apiPlaylist.ts
@@ -1,4 +1,5 @@
 import { URL, KEY, AUTHkey } from "./vars";
+import { getSongs } from "./apiSongs";
 
 export type Tplaylist = {
   playlist_id: number;
@@ -21,8 +22,28 @@ export const getPlaylists = async () => {
   return data;
 };
 
+export const getPlaylistSongsByName = async (name: string | undefined) => {
+  if (!name) throw new Error("Playlist name not found");
+  const options = {
+    method: "GET",
+    headers: {
+      apikey: KEY,
+      Authorization: AUTHkey,
+    },
+  };
+  const response = await fetch(
+    `${URL}/rest/v1/playlists?name=eq.${name}&select=*`,
+    options,
+  );
+  const songs = await getSongs();
+  const playlist: Tplaylist[] = await response.json();
+  return songs.filter(
+    (song) => playlist.at(0)?.songs.indexOf(song.song_id) !== -1,
+  );
+};
+
 export const addPlaylist = async (
-  userID: number,
+  userID: number | undefined,
   playlistName: string,
   songs: number[],
 ) => {
@@ -40,6 +61,23 @@ export const addPlaylist = async (
       songs,
     }),
   };
-  const response = await fetch(`${URL}/rest/v1/liked_songs`, options);
+  if (!userID) throw new Error("User not found");
+  const response = await fetch(`${URL}/rest/v1/playlists`, options);
   if (!response.ok) throw new Error("Failed to create playlist");
+};
+
+export const deletePlaylist = async (playlistID: number) => {
+  const options = {
+    method: "DELETE",
+    headers: {
+      apikey: KEY,
+      Authorization: AUTHkey,
+    },
+  };
+  if (!playlistID) throw new Error("User ID is required");
+  const response = await fetch(
+    `${URL}/rest/v1/playlists?playlist_id=eq.${playlistID}`,
+    options,
+  );
+  if (!response.ok) throw new Error("Failed to delete playlist");
 };
