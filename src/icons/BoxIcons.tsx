169d004e9327445df38b8a4fb8b147be44996171
Mohammad Mehdi Beheshti <mohammadmehdibeheshti@yahoo.com>
2024-02-17 03:16:02 +0330

    update BoxIcons.tsx

--- src/icons/BoxIcons.tsx
+++ src/icons/BoxIcons.tsx
@@ -244,21 +244,6 @@ export function CheckIcon({ className }: Ticon) {
   );
 }
 
-export function RepeatIcon({ className }: Ticon) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      width={defaultSize}
-      height={defaultSize}
-      fill={defaultColor}
-      className={`transition hover:fill-pureWhite ${className}`}
-    >
-      <path d="M21 6h-5v2h4v9H4V8h5v3l5-4-5-4v3H3a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1z"></path>
-    </svg>
-  );
-}
-
 export function TrashcanIcon({ className }: Ticon) {
   return (
     <svg
