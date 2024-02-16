26d38986eae37362d0c5e83258e51a5a5c604769
Mohammad Mehdi Beheshti <mohammadmehdibeheshti@yahoo.com>
2024-02-17 03:15:39 +0330

    update RangeSlider.tsx

--- src/components/ui/RangeSlider.tsx
+++ src/components/ui/RangeSlider.tsx
@@ -1,32 +1,33 @@
-import { InputHTMLAttributes, CSSProperties } from "react";
+import { InputHTMLAttributes } from "react";
 
 interface IrangeSlider extends InputHTMLAttributes<HTMLInputElement> {
   className?: string;
 }
 
 function RangeSlider({ className, ...props }: IrangeSlider) {
-  const styles: CSSProperties = {
-    WebkitAppearance: "none",
-    MozAppearance: "none",
-    appearance: "none",
-    height: "0.5rem",
-    outline: "none",
-    borderRadius: "100rem",
-    background: `linear-gradient(to right, #FEFEFE 0%, #FEFEFE ${props.value}%, #9C9C9C ${props.value}%, #9C9C9C 100%)`,
-    cursor: "ew-resize",
-  };
-
   return (
     <input
       type="range"
+      style={{
+        WebkitAppearance: "none",
+        MozAppearance: "none",
+        appearance: "none",
+        height: "0.5rem",
+        outline: "none",
+        borderRadius: "100rem",
+        background: `linear-gradient(to right, #FEFEFE 0%, #FEFEFE ${props.value}%, #9C9C9C ${props.value}%, #9C9C9C 100%)`,
+        cursor: "ew-resize",
+      }}
       className={`${className}
-      [&::-webkit-slider-thumb]:h-1
-      [&::-webkit-slider-thumb]:w-1
-      [&::-webkit-slider-thumb]:appearance-none
-      [&::-webkit-slider-thumb]:rounded-full
-      [&::-webkit-slider-thumb]:bg-pureWhite
-      `}
-      style={styles}
+    [&::-webkit-slider-thumb]:h-1.2
+    [&::-webkit-slider-thumb]:w-1.2
+    [&::-webkit-slider-thumb]:appearance-none
+    [&::-webkit-slider-thumb]:rounded-full
+    [&::-webkit-slider-thumb]:bg-pureWhite
+    `}
+      min="0"
+      step="1"
+      max="100"
       {...props}
     />
   );
