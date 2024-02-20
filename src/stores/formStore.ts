39e712fbf0475e0476d76396ffa9d1de75567e19
Mohammad Mehdi Beheshti <mohammadmehdibeheshti@yahoo.com>
2024-02-21 01:38:24 +0330

    zustand stores

---
+++ src/stores/formStore.ts
@@ -0,0 +1,41 @@
+import { create } from "zustand";
+
+type TformStore = {
+  form: {
+    signIn: boolean;
+    signUp: boolean;
+    createPlaylist: boolean;
+  };
+  showSignInForm(): void;
+  showSignUpForm(): void;
+  showCreatePlaylistForm(): void;
+  hideSignInForm(): void;
+  hideSignUpForm(): void;
+  hideCreatePlaylistForm(): void;
+};
+
+export const useFormStore = create<TformStore>()((set) => ({
+  form: {
+    signIn: false,
+    signUp: false,
+    createPlaylist: false,
+  },
+  showSignInForm() {
+    set((state) => ({ form: { ...state.form, signIn: true } }));
+  },
+  showSignUpForm() {
+    set((state) => ({ form: { ...state.form, signUp: true } }));
+  },
+  showCreatePlaylistForm() {
+    set((state) => ({ form: { ...state.form, createPlaylist: true } }));
+  },
+  hideSignInForm() {
+    set((state) => ({ form: { ...state.form, signIn: false } }));
+  },
+  hideSignUpForm() {
+    set((state) => ({ form: { ...state.form, signUp: false } }));
+  },
+  hideCreatePlaylistForm() {
+    set((state) => ({ form: { ...state.form, createPlaylist: false } }));
+  },
+}));
