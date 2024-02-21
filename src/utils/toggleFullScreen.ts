b9e851aa40a821c54b4ff0b6ba0c85a00ae92689
Mohammad Mehdi Beheshti <mohammadmehdibeheshti@yahoo.com>
2024-02-22 00:56:34 +0330

    update toggleFullScreen.ts

--- src/utils/toggleFullScreen.ts
+++ src/utils/toggleFullScreen.ts
@@ -1,29 +1,9 @@
 const toggleFullScreen = () => {
-  if (
-    !document.fullscreenElement &&
-    !document.mozFullScreenElement &&
-    !document.webkitFullscreenElement &&
-    !document.msFullscreenElement
-  ) {
-    if (document.documentElement.requestFullscreen) {
-      document.documentElement.requestFullscreen();
-    } else if (document.documentElement.mozRequestFullScreen) {
-      document.documentElement.mozRequestFullScreen();
-    } else if (document.documentElement.webkitRequestFullscreen) {
-      document.documentElement.webkitRequestFullscreen();
-    } else if (document.documentElement.msRequestFullscreen) {
-      document.documentElement.msRequestFullscreen();
-    }
+  if (document.fullscreenElement) {
+    if (document.exitFullscreen) document.exitFullscreen();
   } else {
-    if (document.exitFullscreen) {
-      document.exitFullscreen();
-    } else if (document.mozCancelFullScreen) {
-      document.mozCancelFullScreen();
-    } else if (document.webkitExitFullscreen) {
-      document.webkitExitFullscreen();
-    } else if (document.msExitFullscreen) {
-      document.msExitFullscreen();
-    }
+    if (document.documentElement.requestFullscreen)
+      document.documentElement.requestFullscreen();
   }
 };
 
